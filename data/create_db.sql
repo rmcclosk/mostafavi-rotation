da9afb38d1674dbef8fc59e33f85b40dff3d4514
Rosemary McCloskey <rmcclos@sfu.ca>
2015-01-15 07:23:32 -0800

    Import genes and CpGs into database

--- data/create_db.sql
+++ data/create_db.sql
@@ -1,3 +1,5 @@
+PRAGMA foreign_keys = ON;
+
 CREATE TABLE IF NOT EXISTS CpG (
     IlmnID TEXT PRIMARY KEY,
     Name TEXT,
@@ -20,9 +22,6 @@ CREATE TABLE IF NOT EXISTS CpG (
     Probe_SNPs_10 TEXT,
     Random_Loci TEXT,
     Methyl27_Loci TEXT,
-    UCSC_RefGene_Name TEXT,
-    UCSC_RefGene_Accession TEXT,
-    UCSC_RefGene_Group TEXT,
     UCSC_CpG_Islands_Name TEXT,
     Relation_to_UCSC_CpG_Island TEXT,
     Phantom TEXT,
@@ -34,34 +33,29 @@ CREATE TABLE IF NOT EXISTS CpG (
     DHS TEXT
 );
 
-CREATE TABLE IF NOT EXISTS CpG_Gene (
+CREATE TABLE IF NOT EXISTS refGene (
+    bin INTEGER, 
+    name TEXT PRIMARY KEY,
+    chrom TEXT,
+    strand TEXT,
+    txStart INTEGER,
+    txEnd INTEGER,
+    cdsStart INTEGER,
+    cdsEnd INTEGER,
+    exonCount INTEGER,
+    exonStarts TEXT,
+    exonEnds TEXT,
+    score INTEGER,
+    name2 TEXT,
+    cdsStartStat TEXT,
+    cdsEndStat TEXT,
+    exonFrames TEXT
+);
+
+CREATE TABLE IF NOT EXISTS CpGGene (
     IlmnID TEXT REFERENCES CpG(IlmnID),
     UCSC_RefGene_Name TEXT,
-    UCSC_RefGene_Accession TEXT,
+    UCSC_RefGene_Accession TEXT REFERENCES refGene(name),
     UCSC_RefGene_Group TEXT,
-    PRIMARY KEY (IlmnID, UCSC_RefGene_Name)
+    PRIMARY KEY (IlmnID, UCSC_RefGene_Accession)
 );
-
-CREATE TABLE IF NOT EXISTS refGene (
-    bin smallint(5) unsigned NOT NULL,
-    name varchar(255) NOT NULL,
-    chrom varchar(255) NOT NULL,
-    strand char(1) NOT NULL,
-    txStart int(10) unsigned NOT NULL,
-    txEnd int(10) unsigned NOT NULL,
-    cdsStart int(10) unsigned NOT NULL,
-    cdsEnd int(10) unsigned NOT NULL,
-    exonCount int(10) unsigned NOT NULL,
-    exonStarts longblob NOT NULL,
-    exonEnds longblob NOT NULL,
-    score int(11) DEFAULT NULL,
-    name2 varchar(255) NOT NULL,
-    cdsStartStat enum('none','unk','incmpl','cmpl') NOT NULL,
-    cdsEndStat enum('none','unk','incmpl','cmpl') NOT NULL,
-    exonFrames longblob NOT NULL,
-);
-
-.separator ","
-.import wgEncodeHaibMethyl450CpgIslandDetails.txt cpg
-
-
---
+++ data/import.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+
+import sqlite3
+import csv
+import itertools
+
+con = sqlite3.connect("db.sqlite")
+cur = con.cursor()
+cur.execute("PRAGMA foreign_keys = ON")
+
+def make_insert_query(table, ncols):
+    return "INSERT OR IGNORE INTO {} VALUES ({})".format(table, ",".join("?"*ncols))
+
+# import genes
+refgene_query = make_insert_query("refGene", 16)
+with open("refGene.txt") as f:
+    reader = csv.reader(f, delimiter="\t")
+    cur.executemany(refgene_query, reader)
+    con.commit()
+
+# import CpGs
+cpg_query = make_insert_query("CpG", 30)
+cpg_gene_query = make_insert_query("CpGGene", 4)
+
+with open("wgEncodeHaibMethyl450CpgIslandDetails.txt") as f:
+    while not next(f).startswith("[Assay]"): pass
+
+    reader = csv.reader(f)
+    header = next(reader)
+    ncol = len(header)
+    for row in reader:
+        id = row[0]
+        if not id.startswith("c"): continue
+
+        cpg_values = []
+        cpg_gene_values = [[]]
+        for i in range(ncol):
+            if not header[i].startswith("UCSC_RefGene"):
+                cpg_values.append(row[i])
+            else:
+                cpg_gene_values.append(row[i].split(";"))
+        cur.execute(cpg_query, cpg_values)
+
+        cpg_gene_values = itertools.zip_longest(*cpg_gene_values, fillvalue=id)
+        for v in cpg_gene_values:
+            try:
+                cur.execute(cpg_gene_query, v)
+            except sqlite3.IntegrityError: # old transcript not in refGene
+                pass
+con.close()
