a592b6b40ea8c9bda6e10771071be736a9601e96
Rosemary McCloskey <rmcclosk@node1390.broadinstitute.org>
2015-01-23 00:43:19 -0500

    Information geometric on three variables

--- analysis/infogeo.R
+++ analysis/infogeo.R
@@ -10,16 +10,17 @@ may.cause <- function (x, y) {
 }
 range01 <- function(x){(x-min(x))/(max(x)-min(x))}
 
-vars <- c("tangles_sqrt", "amyloid_sqrt")
-
-con <- dbConnect(SQLite(), "../data/db.sqlite")
-query <- paste("SELECT", paste(vars, collapse=","), "FROM patient")
-res <- dbSendQuery(con, query)
-data <- dbFetch(res)
-. <- dbClearResult(res)
+vars <- c("tangles_sqrt", "amyloid_sqrt", "globcog_random_slope")
+query <- paste("SELECT", paste(vars, collapse=", "), "FROM patient WHERE",
+               paste(vars, collapse=" IS NOT NULL AND "), "IS NOT NULL")
+con <- dbConnect(SQLite(), "../data/db-pheno.sqlite")
+data <- dbGetQuery(con, query)
 . <- dbDisconnect(con)
 
-data <- na.omit(data)
 x <- range01(data$tangles_sqrt)
 y <- range01(data$amyloid_sqrt)
+z <- range01(-data$globcog_random_slope)
+
 may.cause(x, y)
+may.cause(y, z)
+may.cause(x, z)
