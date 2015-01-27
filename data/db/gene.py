54394a5c69393d0d2e1a82ff0a01ea55c53f0f0a
Rosemary McCloskey <rmcclosk@node1378.broadinstitute.org>
2015-01-27 18:02:18 -0500

    Finished gene loading

--- data/db/gene.py
+++ data/db/gene.py
@@ -1,28 +1,9 @@
 #!/usr/bin/env python3
 
-import sys
-import csv
-import logging
-import logging.config
-from _shared import iter_gzip
+import os
 
 def main():
-    logging.config.fileConfig("../logging.conf")
-
-    gene_names = dict(iter_gzip("ensemblToGeneName.txt.gz"))
-    rows = set([])
-    for row in iter_gzip("ensGene.txt.gz"):
-        try:
-            chrom = int(row[2].replace("chr", ""))
-        except ValueError:
-            continue
-        if chrom != 22: continue # DEBUG
-
-        gene_id = row[12].replace("ENSG", "")
-        gene_name = gene_names[row[1]]
-        forward = "TRUE" if row[3] == "+" else "FALSE"
-        rows.add((gene_id, gene_name, chrom, forward))
-    csv.writer(sys.stdout, delimiter="\t").writerows(rows)
+    os.system("zcat ensemblGenes.txt.gz")
 
 if __name__ == "__main__":
     main()
