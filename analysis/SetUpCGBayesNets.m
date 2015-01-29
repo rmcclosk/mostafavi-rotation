24f05e4f01fe555cfacf206290d3955b7e39aea9
Rosemary McCloskey <rmcclosk@nickel.broadinstitute.org>
2015-01-29 17:32:47 -0500

    Drawing of database schema

---
+++ analysis/SetUpCGBayesNets.m
@@ -0,0 +1,8 @@
+function SetUpCGBayesNets( )
+    cgdir = '/home/unix/rmcclosk/packages/cgbayesnets_7_14_14';
+    path(path, cgdir);
+    curdir = cd(cgdir);
+    bnpathscript;
+    tic;
+    cd(curdir);
+end
