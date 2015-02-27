0c134c20e438130397e9969295ad71ec79060083
Rosemary McCloskey <rmcclosk@node1382.broadinstitute.org>
2015-02-27 16:35:32 -0500

    Reorganizing, doing it wrong

--- data/db/chromosome.sql
+++
@@ -1,7 +0,0 @@
-DROP TABLE IF EXISTS chromosome CASCADE;
-CREATE TABLE chromosome (
-    chrom SMALLINT NOT NULL PRIMARY KEY,
-    size INTEGER NOT NULL,
-    CHECK (chrom >= 1 AND chrom <= 22)
-);
-COPY chromosome FROM '../db/chromosome.tsv';
--- data/db/gene.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-import os
-
-def main():
-    os.system("zcat ensemblGenes.txt.gz")
-
-if __name__ == "__main__":
-    main()
