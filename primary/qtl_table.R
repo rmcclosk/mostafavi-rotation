fa529a51a950da4b900a3ae6ab31c2d73949dd20
Rosemary McCloskey <rmcclosk.math@gmail.com>
2015-03-19 12:33:07 -0400

    Added summary of QTLs

--- primary/qtl_table.R
+++ primary/qtl_table.R
@@ -9,50 +9,28 @@ median.iqr <- function (x) {
     sprintf("%.2g [%.2g-%.2g]", median(x), quantile(x, 0.25), quantile(x, 0.75))
 }
 
-#efile <- "../data/residual_gene_expression_expressed_genes_2FPKM100ind.txt"
-#afile <- "../data/chipSeqResiduals.csv"
-#mfile <- "../data/ill450kMeth_all_740_imputed.txt"
-#pfile <- "../data/phenotype_740qc_finalFromLori.txt"
-#gfile <- "../data/transposed_1kG/chr1/chr1.560001.560059.snplist.test.snps.dosage.1kg.trans.txt"
-#
-#id.map <- fread(pfile, select=c("Sample_ID", "projid"), 
-#                colClasses=list(character="projid"))
-#setkey(id.map, Sample_ID)
-#
-#epatients <- fread(efile, select=1)[,gsub(":.*", "", V1)]
-#apatients <- colnames(fread(afile, nrows=0, skip=0))
-#mpatients <- colnames(fread(mfile, nrows=0, skip=0))
-#mpatients <- id.map[mpatients[2:length(mpatients)],projid]
-#
-#gpatients <- fread(gfile, skip=1, select=1)[,gsub("[A-Z]", "", V1)]
-#epatients <- as.character(length(intersect(epatients, gpatients)))
-#apatients <- as.character(length(intersect(apatients, gpatients)))
-#mpatients <- as.character(length(intersect(mpatients, gpatients)))
-#
-#tbl <- data.frame(eQTL=epatients, meQTL=mpatients, aceQTL=apatients)
-#rownames(tbl) <- c("patients")
-#
-#edata <- rbindlist(lapply(paste0("eQTL/chr", 1:22, ".tsv"), fread))
-#mdata <- rbindlist(lapply(paste0("meQTL/chr", 1:22, ".tsv"), fread))
-#adata <- rbindlist(lapply(paste0("aceQTL/chr", 1:22, ".tsv"), fread))
-#
-#raw.stats <- function (data) {
-#    c(`features tested`=data[,length(unique(feature))],
-#      `SNPs tested`=data[,length(unique(snp))],
-#      `total tests done`=nrow(data),
-#      `SNPs per feature`=median.iqr(data[,length(snp),by=feature][,V1]),
-#      `features per SNP`=median.iqr(data[,length(feature),by=snp][,V1]))
-#}
-#data <- list(edata, mdata, adata)
-#raw.data <- do.call(cbind, lapply(data, raw.stats))
-#colnames(raw.data) <- c("eQTL", "meQTL", "aceQTL")
-#tbl <- rbind(tbl, raw.data)
-#
-#rm(data)
-#rm(edata)
-#rm(mdata)
-#rm(adata)
-#gc()
+keeps <- c("feature", "snp")
+edata <- rbindlist(lapply(paste0("eQTL/chr", 1:22, ".tsv"), fread, select=keeps))
+mdata <- rbindlist(lapply(paste0("meQTL/chr", 1:22, ".tsv"), fread, select=keeps))
+adata <- rbindlist(lapply(paste0("aceQTL/chr", 1:22, ".tsv"), fread, select=keeps))
+
+raw.stats <- function (data) {
+    c(`features tested`=data[,length(unique(feature))],
+      `SNPs tested`=data[,length(unique(snp))],
+      `total tests done`=nrow(data),
+      `SNPs per feature`=median.iqr(data[,length(snp),by=feature][,V1]),
+      `features per SNP`=median.iqr(data[,length(feature),by=snp][,V1]))
+}
+data <- list(edata, mdata, adata)
+raw.data <- do.call(cbind, lapply(data, raw.stats))
+colnames(raw.data) <- c("eQTL", "meQTL", "aceQTL")
+tbl <- as.data.frame(raw.data)
+
+rm(data)
+rm(edata)
+rm(mdata)
+rm(adata)
+gc()
 
 ebest <- fread("eQTL/best.tsv")
 mbest <- fread("meQTL/best.tsv")
@@ -75,7 +53,8 @@ best.stats <- function (data) {
 data <- list(ebest, mbest, abest)
 best.data <- do.call(cbind, lapply(data, best.stats))
 colnames(best.data) <- c("eQTL", "meQTL", "aceQTL")
-tbl <- kable(best.data, "markdown")
-cat(tbl, file="qtl_table.md", sep="\t")
-#tbl <- rbind(tbl, best.data)
-#kable(tbl, "markdown")
+#tbl <- kable(best.data, "markdown")
+#cat(tbl, file="qtl_table.md", sep="\n")
+tbl <- rbind(tbl, best.data)
+kbl <- kable(tbl, "markdown")
+cat(kbl, file="qtl_table.md", sep="\n")
