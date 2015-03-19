fe31cc49ed274536b3285503df742fed450d7ea3
Rosemary McCloskey <rmcclosk.math@gmail.com>
2015-03-19 12:19:55 -0400

    Redid some plots with chr1/2

--- plots/qtl_venn.R
+++ plots/qtl_venn.R
@@ -34,35 +34,3 @@ pdf("qtl_venn.pdf")
 dev.off()
 
 quit()
-# TODO: from here on doesn't work anymore
-
-data <- lapply(qtl.types, function (x) {
-    files <- paste0("../primary/", x, "QTL/chr", 1:22, ".tsv")
-    res <- rbindlist(lapply(files, fread))
-    setkey(res, snp, p.value)
-    res[,.SD[1], snp] # best p-value for each snp
-})
-
-sapply(qtl.types, function (qtl.type) {
-    sets <- lapply(data, function (d) {
-        res <- d[best[[qtl.type]],]
-        res[,q.value := p.adjust(p.value, method="fdr")]
-        res[q.value < 0.05, unique(snp)]
-    })
-    
-    png(paste0(qtl.type, "qtl_venn.png"), height=240, width=240)
-    draw.triple.venn(
-        length(sets[[1]]), 
-        length(sets[[2]]), 
-        length(sets[[3]]), 
-        length(intersect(sets[[1]], sets[[2]])),
-        length(intersect(sets[[2]], sets[[3]])),
-        length(intersect(sets[[1]], sets[[3]])),
-        length(Reduce(intersect, sets)),
-        category=paste0(qtl.types, "QTL"),
-        col=brewer.pal(3, "Set2"),
-        fill=brewer.pal(3, "Set2"),
-        alpha=rep(0.3, 3)
-    )
-    dev.off()
-})
