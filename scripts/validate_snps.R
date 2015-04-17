c7640ccedc0a8aacb4147e33af9fd85ab084b680
Rosemary McCloskey <rmcclosk.math@gmail.com>
2015-04-17 15:42:50 -0400

    Fix error in validate_snps.R

--- scripts/validate_snps.R
+++ scripts/validate_snps.R
@@ -9,42 +9,49 @@ library(ggplot2)
 
 source(file=file.path("utils", "load_data.R"))
 
-keeps <- c("PROBE", "SNP", "PERMUTATIONP")
-theirs <- fread(file.path("data", "ROSMAP_brain_rnaseq_best_eQTL.txt"), select=keeps)
-setnames(theirs, "PROBE", "feature")
-setnames(theirs, "SNP", "snp")
-setnames(theirs, "PERMUTATIONP", "adj.p.value")
+cache.file <- file.path("cache", "validate_snps.Rdata")
 
-theirs[,feature := as.integer(gsub("ENSG|[.].*", "", feature))]
-theirs[,q.value := qvalue(adj.p.value)$qvalue]
-theirs[,adj.p.value := NULL]
-theirs <- theirs[q.value < 0.05,]
-setkey(theirs, feature)
-
-keeps <- c("feature", "snp", "q.value")
-classes <- list(character="snp")
-ours <- fread(file.path("results", "eQTL", "PC10.best.tsv"), select=keeps, colClasses=classes)
-
-ours <- ours[!duplicated(feature) & q.value < 0.05,]
-setkey(ours, feature)
-
-qtl.data <- merge(ours, theirs, suffixes=c(".ours", ".theirs"))
-
-snps.needed <- qtl.data[,unique(c(snp.ours, snp.theirs))]
-manifest <- load.manifest()
-setkey(manifest, snp)
-manifest <- manifest[snp %in% snps.needed,]
-
-qtl.data <- qtl.data[snp.ours %in% manifest[,snp] & snp.theirs %in% manifest[,snp],]
-
-patients <- load.patients()
-gdata <- load.gdata(manifest, patients)
+if (!file.exists(cache.file)) {
+    keeps <- c("PROBE", "SNP", "PERMUTATIONP")
+    theirs <- fread(file.path("data", "ROSMAP_brain_rnaseq_best_eQTL.txt"), select=keeps)
+    setnames(theirs, "PROBE", "feature")
+    setnames(theirs, "SNP", "snp")
+    setnames(theirs, "PERMUTATIONP", "adj.p.value")
+    
+    theirs[,feature := as.integer(gsub("ENSG|[.].*", "", feature))]
+    theirs[,q.value := qvalue(adj.p.value)$qvalue]
+    theirs[,adj.p.value := NULL]
+    theirs <- theirs[q.value < 0.05,]
+    setkey(theirs, feature)
+    
+    keeps <- c("feature", "snp", "q.value")
+    classes <- list(character="snp")
+    ours <- fread(file.path("results", "eQTL", "PC10.best.tsv"), select=keeps, colClasses=classes)
+    
+    ours <- ours[!duplicated(feature) & q.value < 0.05,]
+    setkey(ours, feature)
+    
+    qtl.data <- merge(ours, theirs, suffixes=c(".ours", ".theirs"))
+    
+    snps.needed <- qtl.data[,unique(c(snp.ours, snp.theirs))]
+    manifest <- load.manifest()
+    setkey(manifest, snp)
+    manifest <- manifest[snp %in% snps.needed,]
+    
+    qtl.data <- qtl.data[snp.ours %in% manifest[,snp] & snp.theirs %in% manifest[,snp],]
+    
+    patients <- load.patients()
+    gdata <- load.gdata(manifest, patients)
+    save(gdata, qtl.data, file=cache.file)
+} else {
+    load(cache.file)
+}
 
 get.cor.p <- function (snp.ours, snp.theirs) {
     if (snp.ours == snp.theirs) {
         0
     } else {
-        cor.test(rank(gdata[[snp.ours]]), rank(gdata[[snp.theirs]]))$p.value
+        cor.test(rank(gdata[,snp.ours]), rank(gdata[,snp.theirs]))$p.value
     }
 }
 
